3abc5d68b685537442c1709d7c98a91c9e80ad7d
abcd-2804 <144842848+abcd-2804@users.noreply.github.com>
2023-12-08 13:48:46 +0530

    Add files via upload

---
+++ loop square.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 27 16:24:26 2023
+
+@author: 91626
+"""
+
+import turtle
+t= turtle . turtle ()
+for i in range():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    
---
+++ pentagon.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 12 19:32:32 2023
+
+@author: 91626
+"""
+
+import turtle 
+t= turtle . Turtle()
+t.forward (100)
+t.left(70)
+t.forward(100)
+t.left(80)
+t.forward(100)
+t.left(80)
+t.forward(100)
+t.left(70)
+t.forward(100)
